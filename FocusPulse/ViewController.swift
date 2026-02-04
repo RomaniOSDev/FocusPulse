e99d876573c2ec443f9d60472f6a19c925faa15b
Roman <romazan.26@gmail.com>
2026-02-04 12:28:57 +0300

    Initial Commit

---
+++ FocusPulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusPulse
+//
+//  Created by Роман Главацкий on 04.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
